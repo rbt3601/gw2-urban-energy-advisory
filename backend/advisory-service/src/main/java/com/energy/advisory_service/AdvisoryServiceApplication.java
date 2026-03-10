5c262cd61dc8f005ff383973e70b24dcf9280d27
Rajesh0000018 <rajesh.bt07@gmail.com>
2026-03-10 14:27:19 +0000

    Initial commit

---
+++ backend/advisory-service/src/main/java/com/energy/advisory_service/AdvisoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.energy.advisory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvisoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvisoryServiceApplication.class, args);
+	}
+
+}
---
+++ backend/advisory-service/src/test/java/com/energy/advisory_service/AdvisoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energy.advisory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvisoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
